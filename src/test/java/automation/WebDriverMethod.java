63424c8e6de3d1e54cab74fd5b46b03583595210
Vignesh142004 <vigneshranganathan14@gmail.com>
2025-02-21 18:04:30 +0530

    first commit

---
+++ src/test/java/automation/WebDriverMethod.java
@@ -0,0 +1,45 @@
+package automation;
+import java.util.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class WebDriverMethod {
+
+	public static void main(String[] args) 
+	{
+		WebDriver driver=new ChromeDriver();
+		//1)get(url)
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.manage().window().maximize();
+		
+		//2) getTile()
+		String title=driver.getTitle();
+		System.out.println("Title of the Webpage :"+title);
+		
+		//3)getCurrentURl()
+		String currenturl=driver.getCurrentUrl();
+		System.out.println("Current URL of the Webpage :"+currenturl);
+		
+		//4)getPageSource()
+		String sourcecode=driver.getPageSource();
+		System.out.println("Page Source of current website: "+sourcecode);
+		
+		//5)getWindowHandle()
+		String windowId=driver.getWindowHandle();
+		System.out.println("ID of current webpage: "+windowId);
+		
+		//driver.findElement(By.tagName("a")).click();
+		//6)getWindowHandles()
+		Set<String> st =driver.getWindowHandles();
+		System.out.println("ID of multiple browser Windows :");
+		for(String s:st)
+		{
+			System.out.println(s);
+		}
+		
+		
+		
+		driver.quit();
+		
+ 	}
+
+}
--- src/test/java/automation/Xpath.java
+++ src/test/java/automation/Xpath.java
@@ -1,8 +1,47 @@
 package automation;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Xpath {
 
 	public static void main(String[] args) {
+		
+		WebDriver driver=new ChromeDriver();// to open the browser.
+		
+		driver.get("https://demo.opencart.com/");// Launch the webPage.
+		driver.manage().window().maximize();// to maximize the windiow.
+		
+		// XPath with Single Attribute.
+		driver.findElement(By.xpath("//input[@placeholder='Search']")).sendKeys("Laptop");
+		
+		// XPath with multiple Attribute.
+		driver.findElement(By.xpath("//input[@name='search'][@placeholder='Search']")).sendKeys("SmartWatch");
+		
+		// XPath with AND operator
+		driver.findElement(By.xpath("//input[@name='search' and @placeholder='Search']")).sendKeys("SmartWatch  ");
+		
+		// XPath with OR operator.
+		driver.findElement(By.xpath("//input[@name='search' or @placeholder='Search']")).sendKeys("Airpods");
+        
+		// XPath with innerText text().
+		
+		driver.findElement(By.xpath("//a[text()='MacBook']")).click();
+		
+		boolean displaystatus=driver.findElement(By.xpath("//h3[text()='Featured']")).isDisplayed();
+		System.out.println(displaystatus);
+		
+		// XPath with contains()
+		driver.findElement(By.xpath("//input[contains(@placeholder,'Sea')]")).sendKeys("Iphone");
+	
+		// XPath with start-with()
+		driver.findElement(By.xpath("//input[starts-with(@placeholder,'Sea')]")).sendKeys("Airpods");
+		
+		
+		
+		
+		driver.quit(); // to close the webPage.
+		
+		
 		
 
 	}
